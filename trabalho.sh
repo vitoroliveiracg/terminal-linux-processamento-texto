2f80908d68ca890703d14b4ca9a965234dfcf748
Vitor Oliveira <vitor.oliveira.1@aluno.cefet-rj.br>
2025-01-17 16:10:12 -0300

    correcao 2

--- trabalho.sh
+++ trabalho.sh
@@ -12,10 +12,13 @@ grep -o -E '1[01]{0,7}' bruxarias.txt > 1b.txt
 grep -o -E '^1[01]{0,7}$' bruxarias.txt > 1c.txt
 
 #d)
-grep -o -E '[1-9][0-9]*|[0-9]' bruxarias.txt > 1d.txt
+grep -wo -E '[1-9][0-9]*' bruxarias.txt > 1d.txt
 
 #e)
 grep -o -E '[ue]tt' bruxarias.txt > 1e.txt
 
 #f)
-grep -o -E '\b[A-Z]?[a-z]{2,}\b' bruxarias.txt > 1f.txt
+grep -o -E '[A-Z][a-z]{2,}|[a-z]{3,}' bruxarias.txt > 1f.txt
+
+#g)
+grep -o -E '\b[A-Z][A-Z0-9_]*\b' bruxarias.txt > 1g.txt
